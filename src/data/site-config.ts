ed85585196b3c772a7e790714fe3706c5ef44290
Chuck <guitarplrc@yahoo.com>
2024-08-19 15:45:44 -0500

    feat: add rss feed

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,95 +1,100 @@
 export type Image = {
-    src: string;
-    alt?: string;
-};
+	src: string
+	alt?: string
+}
 
 export type Link = {
-    href: string;
-    text: string;
-};
+	href: string
+	text: string
+}
 
 export type SocialLink = Link & {
-    icon: 'dev' | 'github' | 'linkedin';
-};
+	icon: "dev" | "github" | "linkedin"
+}
 
 export type Hero = {
-    title?: string;
-    text?: string;
-    avatar?: Image;
-    backgroundImage?: Image;
-};
+	title?: string
+	text?: string
+	avatar?: Image
+	backgroundImage?: Image
+}
 
 export type SiteConfig = {
-    logo?: Image;
-    title: string;
-    description: string;
-    image?: Image;
-    primaryNavLinks?: Link[];
-    secondaryNavLinks?: Link[];
-    socialLinks?: SocialLink[];
-    hero?: Hero;
-    postsPerPage?: number;
-};
+	logo?: Image
+	title: string
+	description: string
+	image?: Image
+	primaryNavLinks?: Link[]
+	secondaryNavLinks?: Link[]
+	socialLinks?: SocialLink[]
+	hero?: Hero
+	postsPerPage?: number
+}
 
 const siteConfig: SiteConfig = {
-    logo: {
-        src: '/logo.svg',
-        alt: 'My logo'
-    },
-    title: 'Chuck Reynolds',
-    description: 'My blog about web development, specifically front end usually javascript related and other cool stuff.',
-    primaryNavLinks: [
-        {
-            text: 'Home',
-            href: '/'
-        },
-        {
-            text: 'Blog',
-            href: '/blog'
-        },
-        {
-            text: 'About',
-            href: '/about'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        }
-    ],
-    secondaryNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        }
-    ],
-    socialLinks: [
-        {
-            text: 'Go to GitHub repo',
-            href: 'https://github.com/guitarplrc',
-            icon: 'github'
-        },
-        {
-            text: 'Go to LinkedIn profile',
-            href: 'https://www.linkedin.com/in/chuck-reynolds/',
-            icon: 'linkedin'
-        }
-    ],
-    hero: {
-        title: 'Hi there!',
-        text: "My name is Chuck Reynolds. I'm a freelance web developer, author and musician based in Chicago, Il. It's nice to meet you.",
-        avatar: {
-            src: '/avatar.jpg',
-            alt: 'Chuck Reynolds'
-        },
-        backgroundImage: {
-            src: '/hero.webp'
-        }
-    },
-    postsPerPage: 3
-};
+	logo: {
+		src: "/logo.svg",
+		alt: "My logo",
+	},
+	title: "Chuck Reynolds",
+	description:
+		"My blog about web development, specifically front end usually javascript related and other cool stuff.",
+	primaryNavLinks: [
+		{
+			text: "Home",
+			href: "/",
+		},
+		{
+			text: "Blog",
+			href: "/blog",
+		},
+		{
+			text: "About",
+			href: "/about",
+		},
+		{
+			text: "Contact",
+			href: "/contact",
+		},
+		{
+			text: "RSS",
+			href: "/rss.xml",
+		},
+	],
+	secondaryNavLinks: [
+		{
+			text: "About",
+			href: "/about",
+		},
+		{
+			text: "Contact",
+			href: "/contact",
+		},
+	],
+	socialLinks: [
+		{
+			text: "Go to GitHub repo",
+			href: "https://github.com/guitarplrc",
+			icon: "github",
+		},
+		{
+			text: "Go to LinkedIn profile",
+			href: "https://www.linkedin.com/in/chuck-reynolds/",
+			icon: "linkedin",
+		},
+	],
+	hero: {
+		title: "Hi there!",
+		text: "My name is Chuck Reynolds. I'm a freelance web developer, author and musician based in Chicago, Il. It's nice to meet you.",
+		avatar: {
+			src: "/avatar.jpg",
+			alt: "Chuck Reynolds",
+		},
+		backgroundImage: {
+			src: "/hero.webp",
+		},
+	},
+	postsPerPage: 3,
+}
 
-export default siteConfig;
+export default siteConfig
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,34 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import siteConfig from '../data/site-config.ts';
-import { sortPostsByDateDesc } from '../utils/post-utils';
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
 
 export async function GET(context) {
-    const posts = (await getCollection('blog')).sort(sortPostsByDateDesc);
-    return rss({
-        title: siteConfig.title,
-        description: siteConfig.description,
-        site: context.site,
-        items: posts.map((item) => ({
-            title: item.data.title,
-            description: item.data.excerpt,
-            link: `/blog/${item.slug}/`,
-            pubDate: item.data.publishDate.setUTCHours(0)
-        }))
-    });
+	const blog = await getCollection("blog")
+	return rss({
+		// `<title>` field in output xml
+		title: "chuckreynolds.dev",
+		// `<description>` field in output xml
+		description: "the personal blog of Chuck Reynolds, a web developer from Chicago, IL.",
+		// Pull in your project "site" from the endpoint context
+		// https://docs.astro.build/en/reference/api-reference/#contextsite
+		site: context.site,
+		// Path to your custom XSL stylesheet
+		stylesheet: "/rss/styles.xsl",
+		// Array of `<item>`s in output xml
+		// See "Generating items" section for examples using content collections and glob imports
+		items: blog
+			.slice(0, 10)
+			.map(post => ({
+				title: post.data.title,
+				pubDate: post.data.publishDate,
+				description: post.data.excerpt,
+				// content: sanitizeHtml(parser.render(post.body), {
+				// 	allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+				// }),
+				link: `/blog/${post.slug}/`,
+				...post.data,
+			}))
+			.sort((a, b) => new Date(b.publishDate) - new Date(a.publishDate)),
+		// (optional) inject custom xml
+		customData: `<language>en-us</language>`,
+	})
 }
